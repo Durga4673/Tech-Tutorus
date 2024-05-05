bfc9e7ae36b0a47f7d2c5d8e2d9555ccfde5abc9
Durga prasad <durgaprasad.kurada@peopletech.com>
2024-05-05 12:05:05 +0530

    git links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://script.google.com/macros/s/AKfycbwoL7A03UEfLGTLTDoUYwSYLki0JPy3QCvmw7kCOdkEwWf_PM4DW1T_09ufkmcnUHAJ/exec',
-    CONNECTIONURL : 'https://sheet.best/api/sheets/c77e97c0-b97a-497a-b777-c40fabcb90fe',
+    CONNECTIONURL : 'https://sheet.best/api/sheets/4b683da5-26a4-47e5-b0cf-9eac4dec14b3',
   };
